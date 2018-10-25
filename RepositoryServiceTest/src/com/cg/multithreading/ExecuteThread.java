2ef093372bb3ed402cb7f174da2ff713fb949ef8
E0227472 <30948299+E0227472@users.noreply.github.com>
2018-10-25 09:42:14 +0800

    moreThreadsSpawned

--- RepositoryServiceTest/src/com/cg/multithreading/ExecuteThread.java
+++ RepositoryServiceTest/src/com/cg/multithreading/ExecuteThread.java
@@ -8,5 +8,21 @@ public class ExecuteThread {
 		t.start();
 		Thread t1 = new Thread(tp);
 		t1.start();
+		Thread t3 = new Thread(tp);
+		t3.start();
+		Thread t4 = new Thread(tp);
+		t4.start();
+		Thread t5 = new Thread(tp);
+		t5.start();
+		Thread t6 = new Thread(tp);
+		t6.start();
+		Thread t7 = new Thread(tp);
+		t7.start();
+		Thread t8 = new Thread(tp);
+		t8.start();
+		Thread t9 = new Thread(tp);
+		t9.start();
+		Thread t10 = new Thread(tp);
+		t10.start();
 	}
 }
